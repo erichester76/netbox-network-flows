90a2771fd09c1fff0ec8a195cd3116da7ab06525
Eric Hester <eric.hester@umbrella.tech>
2025-02-22 14:34:21 -0500

    .

--- netbox_network_flows/tables.py
+++ netbox_network_flows/tables.py
@@ -1,18 +1,18 @@
-from django import forms
+import django_tables2 as tables
 from .models import TrafficFlow
-from netbox.forms import NetBoxModelForm
-from virtualization.models import VirtualMachine
+from netbox.tables import NetBoxTable, columns
 
-class TrafficFlowForm(NetBoxModelForm):
-    virtual_machine = forms.ModelChoiceField(
-        queryset=VirtualMachine.objects.all(),
-        required=False,
-        label='Virtual Machine'
-    )
+class TrafficFlowTable(NetBoxTable):
+    src_ip = tables.Column(verbose_name='Source IP')
+    dst_ip = tables.Column(verbose_name='Destination IP')
+    protocol = tables.Column()
+    src_port = tables.Column(verbose_name='Source Port')
+    dst_port = tables.Column(verbose_name='Destination Port')
+    server_id = tables.Column(verbose_name='Server ID')
+    virtual_machine = tables.Column(linkify=True)
+    timestamp = columns.DateTimeColumn()
 
-    class Meta:
+    class Meta(NetBoxTable.Meta):
         model = TrafficFlow
         fields = ('src_ip', 'dst_ip', 'protocol', 'src_port', 'dst_port', 'server_id', 'virtual_machine', 'timestamp')
-        widgets = {
-            'timestamp': forms.NumberInput(attrs={'step': '0.01'}),
-        }
+        default_columns = ('src_ip', 'dst_ip', 'protocol', 'src_port', 'dst_port', 'server_id', 'virtual_machine')
