c8b5b9b00d9695afad5321476aaa80f2abeba607
Eric Hester <eric.hester@umbrella.tech>
2025-02-22 13:56:06 -0500

    naming fix

--- netbox_network_flows/__init__.py
+++ netbox_network_flows/__init__.py
@@ -2,13 +2,13 @@ from netbox.plugins import PluginConfig, PluginMenuItem, PluginMenu
 from django.urls import reverse
 
 class TrafficFlowsConfig(PluginConfig):
-    name = 'traffic_flows'
+    name = 'netbox_network_flows'
     verbose_name = 'Traffic Flows'
     description = 'Manage network traffic flows between servers'
     version = '0.1'
     author = 'Eric Hester'
     author_email = 'hester1@clemson.edu'
-    base_url = 'traffic-flows'
+    base_url = 'flows'
     min_version = '4.1.0'
     max_version = '4.1.99'
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='traffic_flows',
+    name='netbox_network_flows',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
