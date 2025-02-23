62d1724a35b0f3cef3dc7ffd4a088941935a4512
Eric Hester <eric.hester@umbrella.tech>
2025-02-23 16:36:47 -0500

    .

--- netbox_network_flows/views.py
+++ netbox_network_flows/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from netbox.views import generic
-from netbox.registry import register_model_view
-from netbox.views.generic.base import ViewTab
+from netbox.views import ViewTab, register_model_view
 from virtualization.models import VirtualMachine
 from dcim.models import Device
 from netbox_network_flows.models import TrafficFlow
