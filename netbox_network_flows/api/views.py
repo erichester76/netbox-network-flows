7605addeeed02bea696c8ec8d44df3ee8c077a2e
Eric Hester <eric.hester@umbrella.tech>
2025-02-22 21:28:07 -0500

    .

--- netbox_network_flows/api/views.py
+++ netbox_network_flows/api/views.py
@@ -1,20 +1,29 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from ..models import TrafficFlow
-from .serializers import TrafficFlowSerializer
+from traffic_flows.models import TrafficFlow
+from traffic_flows.api.serializers import TrafficFlowSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TrafficFlowViewSet(viewsets.ModelViewSet):
     queryset = TrafficFlow.objects.all()
     serializer_class = TrafficFlowSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['src_ip', 'dst_ip', 'protocol', 'src_port', 'dst_port', 'server_id']
 
     def create(self, request, *args, **kwargs):
         if isinstance(request.data, dict) and 'flows' in request.data:
             flows = request.data['flows']
             server_id = request.data.get('server_id', '')
+            created_flows = []
             for flow in flows:
                 flow['server_id'] = server_id
                 serializer = self.get_serializer(data=flow)
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
-            return Response({"status": "success"}, status=201)
-        return super().create(request, *args, **kwargs)
+                created_flows.append(serializer.data)
+            return Response(created_flows, status=201)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=201, headers=headers)
