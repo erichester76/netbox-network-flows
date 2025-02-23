5c56df92f9751143f594fe88dc5ea03da09effef
Eric Hester <eric.hester@umbrella.tech>
2025-02-23 09:23:33 -0500

    .

--- netbox_network_flows/api/views.py
+++ netbox_network_flows/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from ..models import TrafficFlow
-from serializers import TrafficFlowSerializer
+from .serializers import TrafficFlowSerializer
 
 class TrafficFlowViewSet(viewsets.ModelViewSet):
     """
