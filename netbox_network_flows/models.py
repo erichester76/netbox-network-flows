53d259c5758bf28035fc0ce211bc2d474a5276a1
Eric Hester <eric.hester@umbrella.tech>
2025-02-23 15:23:51 -0500

    .

--- netbox_network_flows/models.py
+++ netbox_network_flows/models.py
@@ -5,9 +5,6 @@ from netbox.models import NetBoxModel
 from ipam.models import IPAddress
 from virtualization.models import VirtualMachine, VMInterface
 from dcim.models import Device, Interface
-import logging
-
-logger = logging.getLogger(__name__)
 
 class TrafficFlow(NetBoxModel):
     src_ip = models.CharField(max_length=45)
@@ -30,60 +27,42 @@ class TrafficFlow(NetBoxModel):
         # Resolve src_ip via IPAddress
         if not self.src_content_type or not self.src_object_id:
             ip_part = self.src_ip.split('/')[0]
-            logger.debug(f"Looking up src_ip: {ip_part}")
-            ip = IPAddress.objects.filter(address__net_contains_or_equals=ip_part).first()
+            ip = IPAddress.objects.filter(address__contains_or_equals=ip_part).first()
             if ip:
-                logger.debug(f"Found IPAddress for src_ip: {ip.address}, assigned_to={ip.assigned_object}")
                 assigned_obj = ip.assigned_object
                 if assigned_obj:
                     if isinstance(assigned_obj, VMInterface):
                         self.src_content_type = ContentType.objects.get_for_model(VirtualMachine)
                         self.src_object_id = assigned_obj.virtual_machine.pk
-                        logger.debug(f"Assigned src to VirtualMachine: {assigned_obj.virtual_machine}")
                     elif isinstance(assigned_obj, Interface):
                         self.src_content_type = ContentType.objects.get_for_model(Device)
                         self.src_object_id = assigned_obj.device.pk
-                        logger.debug(f"Assigned src to Device: {assigned_obj.device}")
                     else:
-                        logger.warning(f"Unexpected assigned_object type for src_ip: {type(assigned_obj)}")
                         self.src_content_type = ContentType.objects.get_for_model(IPAddress)
                         self.src_object_id = ip.pk
-                        logger.debug(f"Assigned src to IPAddress: {ip}")
                 else:
                     self.src_content_type = ContentType.objects.get_for_model(IPAddress)
                     self.src_object_id = ip.pk
-                    logger.debug(f"Assigned src to IPAddress: {ip}")
-            else:
-                logger.debug(f"No IPAddress found for src_ip: {ip_part}")
 
         # Resolve dst_ip via IPAddress
         if not self.dst_content_type or not self.dst_object_id:
             ip_part = self.dst_ip.split('/')[0]
-            logger.debug(f"Looking up dst_ip: {ip_part}")
-            ip = IPAddress.objects.filter(address__net_contains_or_equals=ip_part).first()
+            ip = IPAddress.objects.filter(address__contains_or_equals=ip_part).first()
             if ip:
-                logger.debug(f"Found IPAddress for dst_ip: {ip.address}, assigned_to={ip.assigned_object}")
                 assigned_obj = ip.assigned_object
                 if assigned_obj:
                     if isinstance(assigned_obj, VMInterface):
                         self.dst_content_type = ContentType.objects.get_for_model(VirtualMachine)
                         self.dst_object_id = assigned_obj.virtual_machine.pk
-                        logger.debug(f"Assigned dst to VirtualMachine: {assigned_obj.virtual_machine}")
                     elif isinstance(assigned_obj, Interface):
                         self.dst_content_type = ContentType.objects.get_for_model(Device)
                         self.dst_object_id = assigned_obj.device.pk
-                        logger.debug(f"Assigned dst to Device: {assigned_obj.device}")
                     else:
-                        logger.warning(f"Unexpected assigned_object type for dst_ip: {type(assigned_obj)}")
                         self.dst_content_type = ContentType.objects.get_for_model(IPAddress)
                         self.dst_object_id = ip.pk
-                        logger.debug(f"Assigned dst to IPAddress: {ip}")
                 else:
                     self.dst_content_type = ContentType.objects.get_for_model(IPAddress)
                     self.dst_object_id = ip.pk
-                    logger.debug(f"Assigned dst to IPAddress: {ip}")
-            else:
-                logger.debug(f"No IPAddress found for dst_ip: {ip_part}")
 
         super().save(*args, **kwargs)
 
