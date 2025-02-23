70dc437294a9f13fe90118adec76228cd0fab18a
Eric Hester <eric.hester@umbrella.tech>
2025-02-22 21:45:32 -0500

    .

--- netbox_network_flows/navigation.py
+++ netbox_network_flows/navigation.py
@@ -2,23 +2,23 @@ from netbox.plugins import PluginMenuItem, PluginMenuButton, PluginMenu
 
 items = (
     PluginMenuItem(
-        link='plugins:netbox_network_flows:flow_list',
+        link='plugins:netbox_network_flows:trafficflow_list',
         link_text='Flows',
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_network_flows:flow_add',
+                link='plugins:netbox_network_flows:trafficflow_add',
                 title='Add',
                 icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_network_flows.add_trafficflow']
+                permissions=['netbox_network_flows.add_flow']
             ),
             PluginMenuButton(
-                link='plugins:netbox_network_flows:flow_import',
+                link='plugins:netbox_network_flows:trafficflow_import',
                 title='Import',
                 icon_class='mdi mdi-upload',
-                permissions=['netbox_network_flows.add_trafficflow']
+                permissions=['netbox_network_flows.add_low']
             ),
         ),
-        permissions=['netbox_network_flows.view_trafficflow']
+        permissions=['netbox_network_flows.view_flow']
     ),
 )
 
--- netbox_network_flows/urls.py
+++ netbox_network_flows/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .views import TrafficFlowListView, TrafficFlowEditView, TrafficFlowImportView
 
 urlpatterns = [
-    path('flows/', TrafficFlowListView.as_view(), name='flow_list'),
-    path('flows/add/', TrafficFlowEditView.as_view(), name='flow_add'),
-    path('flows/<int:pk>/edit/', TrafficFlowEditView.as_view(), name='flow_edit'),
-    path('flows/import/', TrafficFlowImportView.as_view(), name='flow_import'),
+    path('flows/', TrafficFlowListView.as_view(), name='trafficflow_list'),
+    path('flows/add/', TrafficFlowEditView.as_view(), name='trafficflow_add'),
+    path('flows/<int:pk>/edit/', TrafficFlowEditView.as_view(), name='trafficflow_edit'),
+    path('flows/import/', TrafficFlowImportView.as_view(), name='trafficflow_import'),
 ]
