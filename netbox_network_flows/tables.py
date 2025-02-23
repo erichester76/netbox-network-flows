2ce41895da54782b2223ab2674f6e186350b2eda
Eric Hester <eric.hester@umbrella.tech>
2025-02-23 15:31:34 -0500

    .

--- netbox_network_flows/tables.py
+++ netbox_network_flows/tables.py
@@ -5,7 +5,16 @@ from django.urls import reverse
 
 class TrafficFlowTable(NetBoxTable):
     timestamp = columns.DateTimeColumn()
-
+    src_ip = tables.Column(verbose_name='Source IP')
+    src_object = tables.Column(
+        verbose_name='Source Object',
+        linkify=True
+    )
+    dst_ip = tables.Column(verbose_name='Destination IP')
+    dst_object = tables.Column(
+        verbose_name='Destination Object',
+        linkify=True
+    )
     class Meta(NetBoxTable.Meta):
         model = TrafficFlow
         fields = ('pk', 'id', 'src_ip', 'src_object', 'dst_ip', 'dst_object', 'protocol', 'service_port', 'server_id', 'timestamp')
