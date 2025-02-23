ddcd55780af551adda7961c9837a88bfdcd8e516
Eric Hester <eric.hester@umbrella.tech>
2025-02-23 13:01:43 -0500

    .

--- netbox_network_flows/tables.py
+++ netbox_network_flows/tables.py
@@ -6,12 +6,12 @@ from django.urls import reverse
 class TrafficFlowTable(NetBoxTable):
     src_ip = tables.Column(
         verbose_name='Source IP',
-        accessor='src_ip',  # Ensures the raw value is used
+        accessor='src_ip',  
         linkify=lambda record: reverse('ipam:ipaddress', kwargs={'pk': record.src_object_id}) if record.src_content_type and record.src_content_type.model == 'ipaddress' else None
     )
     src_object = tables.Column(
         verbose_name='Source Object',
-        linkify=True  # Should work for GFKs to VM/Device/IPAddress
+        linkify=True  #
     )
     dst_ip = tables.Column(
         verbose_name='Destination IP',
@@ -20,12 +20,12 @@ class TrafficFlowTable(NetBoxTable):
     )
     dst_object = tables.Column(
         verbose_name='Dest Object',
-        linkify=True  # Should work for GFKs to VM/Device/IPAddress
+        linkify=True  
     )
 
     timestamp = columns.DateTimeColumn()
 
     class Meta(NetBoxTable.Meta):
         model = TrafficFlow
-        fields = ('id', 'src_ip', 'src_object', 'dst_ip', 'dst_object', 'protocol', 'service_port', 'server_id', 'timestamp')
-        default_columns = ('id', 'src_ip', 'src_object', 'dst_ip', 'dst_object', 'protocol', 'service_port')
+        fields = ('pk', 'src_ip', 'src_object', 'dst_ip', 'dst_object', 'protocol', 'service_port', 'server_id', 'timestamp')
+        default_columns = ('pk', 'src_ip', 'src_object', 'dst_ip', 'dst_object', 'protocol', 'service_port')
