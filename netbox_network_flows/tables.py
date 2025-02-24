63ba6f7088adbcc9041034f37f096bae32152bd2
Eric Hester <eric.hester@umbrella.tech>
2025-02-23 19:31:20 -0500

    .

--- netbox_network_flows/tables.py
+++ netbox_network_flows/tables.py
@@ -7,8 +7,8 @@ class TrafficFlowTable(NetBoxTable):
     timestamp = columns.DateTimeColumn()
     src_ip = tables.Column(verbose_name='Source IP')
     dst_ip = tables.Column(verbose_name='Destination IP')
-    src_object = tables.Column(verbose_name='Source Object', linkify=True)
-    dst_object = tables.Column(verbose_name='Destination Object', linkify=True)
+    # src_object = tables.Column(verbose_name='Source Object', linkify=True)
+    # dst_object = tables.Column(verbose_name='Destination Object', linkify=True)
     
     class Meta(NetBoxTable.Meta):
         model = TrafficFlow
