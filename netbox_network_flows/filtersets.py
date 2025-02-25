46b2914a54755011a32d871e39090266e5a6e337
Eric Hester <eric.hester@umbrella.tech>
2025-02-25 00:53:45 -0500

    .

--- netbox_network_flows/filtersets.py
+++ netbox_network_flows/filtersets.py
@@ -1,13 +1,22 @@
-from netbox.filtersets import NetBoxModelFilterSet
+import django_filters
 from netbox_network_flows.models import ServiceEndpoint, TrafficFlow
 
-class ServiceEndpointFilterSet(NetBoxModelFilterSet):
+class ServiceEndpointFilterSet(django_filters.FilterSet):
+    service_port = django_filters.NumberFilter(field_name='service_port', lookup_expr='exact')
+    process_name = django_filters.CharFilter(field_name='process_name', lookup_expr='iexact')
+    application_name = django_filters.CharFilter(field_name='application_name', lookup_expr='icontains')
 
     class Meta:
         model = ServiceEndpoint
         fields = ['service_port', 'process_name', 'application_name']
 
-class TrafficFlowFilterSet(NetBoxModelFilterSet):
+class TrafficFlowFilterSet(django_filters.FilterSet):
+    src_ip = django_filters.CharFilter(field_name='src_ip', lookup_expr='exact')
+    dst_ip = django_filters.CharFilter(field_name='dst_ip', lookup_expr='exact')
+    protocol = django_filters.CharFilter(field_name='protocol', lookup_expr='exact')
+    service_port = django_filters.NumberFilter(field_name='service_port', lookup_expr='exact')
+    server_id = django_filters.CharFilter(field_name='server_id', lookup_expr='exact')
+    timestamp = django_filters.NumberFilter(field_name='timestamp', lookup_expr='exact')
 
     class Meta:
         model = TrafficFlow
