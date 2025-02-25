52dad95a8cc259b031cd5952796744cc7f2d3c09
Eric Hester <eric.hester@umbrella.tech>
2025-02-25 01:15:21 -0500

    .

--- netbox_network_flows/navigation.py
+++ netbox_network_flows/navigation.py
@@ -1,44 +1,44 @@
-# netbox_network_flows/navigation.py
+# plugins:netbox_network_flows/navigation.py
 from netbox.plugins import PluginMenu, PluginMenuItem, PluginMenuButton
 
 items = (
     PluginMenuItem(
-        link='netbox_network_flows:trafficflow_list',
+        link='plugins:netbox_network_flows:trafficflow_list',
         link_text='Flows',
         buttons=(
             PluginMenuButton(
-                link='netbox_network_flows:trafficflow_add',
+                link='plugins:netbox_network_flows:trafficflow_add',
                 title='Add',
                 icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_network_flows.add_flow']
+                permissions=['plugins:netbox_network_flows.add_flow']
             ),
             PluginMenuButton(
-                link='netbox_network_flows:trafficflow_import',
+                link='plugins:netbox_network_flows:trafficflow_import',
                 title='Import',
                 icon_class='mdi mdi-upload',
-                permissions=['netbox_network_flows.add_flow']
+                permissions=['plugins:netbox_network_flows.add_flow']
             ),
         ),
-        permissions=['netbox_network_flows.view_flow']
+        permissions=['plugins:netbox_network_flows.view_flow']
     ),
     PluginMenuItem(
-        link='netbox_network_flows:serviceendpoint_list',
+        link='plugins:netbox_network_flows:serviceendpoint_list',
         link_text='Service Endpoints',
         buttons=(
             PluginMenuButton(
-                link='netbox_network_flows:serviceendpoint_add',
+                link='plugins:netbox_network_flows:serviceendpoint_add',
                 title='Add',
                 icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_network_flows.add_serviceendpoint']
+                permissions=['plugins:netbox_network_flows.add_serviceendpoint']
             ),
             PluginMenuButton(
-                link='netbox_network_flows:serviceendpoint_import',
+                link='plugins:netbox_network_flows:serviceendpoint_import',
                 title='Import',
                 icon_class='mdi mdi-upload',
-                permissions=['netbox_network_flows.add_serviceendpoint']
+                permissions=['plugins:netbox_network_flows.add_serviceendpoint']
             ),
         ),
-        permissions=['netbox_network_flows.view_serviceendpoint']
+        permissions=['plugins:netbox_network_flows.view_serviceendpoint']
     ),
 )
 
